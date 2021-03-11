0f6f9bc87ca93e247875bac5b73938a5762c0c78
Bubbus <splambob@googlemail.com>
2021-03-11 20:52:00 +0000

    View-distance editor is now unrestricted by default.

--- configuration/viewDistanceEditor.hpp
+++ configuration/viewDistanceEditor.hpp
@@ -4,9 +4,9 @@
 #define ENABLE_VIEWDISTANCE_EDITOR 1
 
 // When enabled, only units with the following variable can use the view-distande editor.  Use for pilots, snipers, tankers etc...
-// this setVariable ["f_var_allowViewDistanceEditing", true];
+// this setVariable ["f_var_allowViewDistanceEditing", true, true];
 // To allow everyone to edit view distance, comment-out or delete the line below INSTEAD of setting it to 0.
-#define VIEWDISTANCE_EDITOR_WHITELISTING 1
+// #define VIEWDISTANCE_EDITOR_WHITELISTING 1
 
 // Enable players to access the lowest-quality terrain detail setting.  This can help with frame-rate, but also disables grass and can therefore be a huge gameplay advantage.
 // To prevent players from disabling grass, comment-out or delete the line below INSTEAD of setting it to 0.
