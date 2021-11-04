9c47b4de83526e4c27cbafcc14e2dc7140092c43
Joecuronium <83242905+Joecuronium@users.noreply.github.com>
2021-11-04 19:31:25 +0100

    more cleanup

--- components/zeus_ui/functions.hpp
+++ components/zeus_ui/functions.hpp
@@ -2,10 +2,8 @@ class zeusui
 {
 	file = "components\zeus_ui";
 	class compileLists{postInit = 1;};
-	class detectZeusDisplay{};
 	class server_spawnGroup{};
 	class zeusUI{};
-	class isZeusRemoteControlling{};
 	class addToCurator{};
 	class addZeusActions{};
 	class exitZeus{};
