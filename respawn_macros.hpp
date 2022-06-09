e6e541e8159324af3476c18a34f4672787a89630
Bubbus <splambob@googlemail.com>
2022-06-09 22:37:27 +0100

    imagine no tab indents

--- respawn_macros.hpp
+++ respawn_macros.hpp
@@ -2,10 +2,10 @@
 #define RESPAWN_TICKETS "Tickets"
 #define RESPAWN_TIMED "CAFE_Timed"
 
-#define RESPAWN_MODE_TIMED       			{"MenuPosition", "Spectator", RESPAWN_TIMED, "CAFE_Loadout", "CAFE_Squad"}
-#define RESPAWN_MODE_TIMED_WAVES 			{"MenuPosition", "Spectator", RESPAWN_TIMED_WAVE, "CAFE_Loadout", "CAFE_Squad"}
-#define RESPAWN_MODE_TICKETS 				{"MenuPosition", "Spectator", RESPAWN_TICKETS, "CAFE_Loadout", "CAFE_Squad"}
-#define RESPAWN_MODE_TIMED_WAVES_TICKETS 	{"MenuPosition", "Spectator", RESPAWN_TIMED_WAVE, RESPAWN_TICKETS, "CAFE_Loadout", "CAFE_Squad"}
+#define RESPAWN_MODE_TIMED                  {"MenuPosition", "Spectator", RESPAWN_TIMED, "CAFE_Loadout", "CAFE_Squad"}
+#define RESPAWN_MODE_TIMED_WAVES            {"MenuPosition", "Spectator", RESPAWN_TIMED_WAVE, "CAFE_Loadout", "CAFE_Squad"}
+#define RESPAWN_MODE_TICKETS                {"MenuPosition", "Spectator", RESPAWN_TICKETS, "CAFE_Loadout", "CAFE_Squad"}
+#define RESPAWN_MODE_TIMED_WAVES_TICKETS    {"MenuPosition", "Spectator", RESPAWN_TIMED_WAVE, RESPAWN_TICKETS, "CAFE_Loadout", "CAFE_Squad"}
 
 #define RESPAWN_DEFAULTS "MenuPosition", "Spectator", "CAFE_Loadout", "CAFE_Squad"
 
