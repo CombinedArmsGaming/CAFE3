f7c957b068972baed7c88b9310e375f46d12426b
Emma <emma.github@empollak.com>
2026-02-23 13:13:32 -0500

    Loadout picker base

---
+++ components/gearScript/ui_macros.hpp
@@ -0,0 +1,5 @@
+#include "../../ui_macros.hpp"
+
+#define IDD_LOADOUTPICKER_DIALOG 416451
+
+#define IDC_CURRENTLOADOUT_TEXT 4000
--- components/respawn/ui_macros.hpp
+++ components/respawn/ui_macros.hpp
@@ -1,3 +1,14 @@
 #include "../../ui_macros.hpp"
 
 #define IDD_SPAWNPICKER_DIALOG 12154
+
+#define IDC_TICKETS_TEXT 1003
+
+#define IDC_DEATH_TIMER 1004
+
+#define IDC_WAVE_TEXT 1005
+
+// Info box selector buttons
+#define IDC_LOCATION_BUTTON 1006
+#define IDC_GROUP_BUTTON 1007
+#define IDC_LOADOUT_BUTTON 1008
